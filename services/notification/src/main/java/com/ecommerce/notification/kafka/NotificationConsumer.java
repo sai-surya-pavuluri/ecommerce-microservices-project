ebfebc529bedde169ff4b354d626fd71baa8a6d0
Sai Surya Pavuluri <sai.surya.p@ibm.com>
2025-12-23 19:27:34 +0000

    Notification microservice implemented

---
+++ services/notification/src/main/java/com/ecommerce/notification/kafka/NotificationConsumer.java
@@ -0,0 +1,78 @@
+package com.ecommerce.notification.kafka;
+
+import com.ecommerce.notification.dto.OrderConfirmation;
+import com.ecommerce.notification.dto.PaymentConfirmation;
+import com.ecommerce.notification.enums.NotificationType;
+import com.ecommerce.notification.model.Notification;
+import com.ecommerce.notification.repository.NotificationRepository;
+import com.ecommerce.notification.service.EmailService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+
+import static java.lang.String.format;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class NotificationConsumer {
+
+    private final NotificationRepository notificationRepository;
+    private final EmailService emailService;
+
+    @KafkaListener(topics = "payment-topic")
+    public void consumePaymentNotification(PaymentConfirmation paymentConfirmation) {
+        log.info(format("Consuming the message from payment-topic payment Topic:: %s:", paymentConfirmation));
+        notificationRepository.save(
+                Notification.builder()
+                        .type(NotificationType.PAYMENT_CONFIRMATION)
+                        .notificationDate(LocalDateTime.now())
+                        .paymentConfirmation(paymentConfirmation)
+                        .build()
+        );
+
+        try {
+            emailService.sendPaymentSuccessEmail(
+                    paymentConfirmation.customerEmail(),
+                    paymentConfirmation.customerFirstname(),
+                    paymentConfirmation.amount(),
+                    paymentConfirmation.orderReference()
+            );
+        }
+        catch (Exception e) {
+            log.info("Error while sending email to payment-topic payment Topic:: ", e);
+        }
+
+
+
+    }
+
+    @KafkaListener(topics = "order-topic")
+    public void consumeOrderNotification(OrderConfirmation orderConfirmation) {
+        log.info(format("Consuming the message from order-topic order Topic:: %s:", orderConfirmation));
+        notificationRepository.save(
+                Notification.builder()
+                        .type(NotificationType.ORDER_CONFIRMATION)
+                        .notificationDate(LocalDateTime.now())
+                        .orderConfirmation(orderConfirmation)
+                        .build()
+        );
+
+        try{
+            emailService.sendOrderConfirmationEmail(
+                    orderConfirmation.customer().email(),
+                    orderConfirmation.customer().firstname(),
+                    orderConfirmation.totalAmount(),
+                    orderConfirmation.orderReference(),
+                    orderConfirmation.products()
+            );
+        } catch(Exception e){
+            log.info("Error while sending email to order-topic order Topic:: ", e);
+        }
+
+
+    }
+}
---
+++ services/notification/src/main/java/com/ecommerce/notification/model/Notification.java
@@ -0,0 +1,31 @@
+package com.ecommerce.notification.model;
+
+import com.ecommerce.notification.dto.OrderConfirmation;
+import com.ecommerce.notification.dto.PaymentConfirmation;
+import com.ecommerce.notification.enums.NotificationType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Document
+public class Notification {
+
+    @Id
+    private String id;
+    private NotificationType type;
+    private LocalDateTime notificationDate;
+    private OrderConfirmation orderConfirmation;
+    private PaymentConfirmation paymentConfirmation;
+}
